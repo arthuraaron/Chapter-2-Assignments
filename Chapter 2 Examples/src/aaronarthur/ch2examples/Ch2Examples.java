9e59e7fbc431bf9529d7219e6f1dd1027e63aa3f
aaron.d.arthur <aaron.d.arthur@CLC-LAB209-10.TCSD.tooelesd.org>
2018-10-04 14:12:12 -0600

    10/4

---
+++ Chapter 2 Examples/src/aaronarthur/ch2examples/Ch2Examples.java
@@ -0,0 +1,13 @@
+package aaronarthur.ch2examples;
+//Aaron Arthur 9/18 Chapter 2 Examples
+
+public class Ch2Examples {
+
+	public static void main(String[] args) {
+		// Print text
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java\nAnother Line\n\n\nTest");
+		System.out.println((10.4+33.3)*(105-19));
+	}
+
+}
